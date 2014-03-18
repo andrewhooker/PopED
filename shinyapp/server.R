11efc3ccef78dad315125fd904c0f480cd7a02df
Andrew Hooker <andrew.c.hooker@gmail.com>
2014-03-18 15:57:26 +0100

    Improving the GUI

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -1,5 +1,6 @@
 library(shiny)
 #library(datasets)
+source("MODEL.2.PK.one.comp.oral.R")
 
 # We tweak the "am" field to have nicer factor labels. Since this doesn't
 # rely on any user inputs we can do this once at startup and then use the
@@ -12,15 +13,42 @@ shinyServer(function(input, output) {
   
   # Compute the forumla text in a reactive expression since it is 
   # shared by the output$caption and output$mpgPlot expressions
+  updateDesign <- reactive({
+    xt_txt <- input$xt
+    xt <-  eval(parse(text=paste("c(",xt_txt,")")))
+    return(list(xt=xt))
+  })
+  
+  updateModel_2 <- reactive({
+    if(input$model=="one.comp.oral"){ 
+      ff <- "PK.one.cpt.oral.ff.model"
+      sfg  <- "PK.one.cpt.oral.params.trans2"
+      bpop_vals <- c(V=72.8,KA=0.25,CL=3.75,Favail=0.9)
+      d_vals <- c(V=0.09,KA=0.09,CL=0.25^2)
+      sigma_vals <- diag(c(0.04,5e-6))
+      notfixed_bpop <- c(1,1,1,0)
+      notfixed_sigma <- c(0,0)
+      notfixed_d <- c(1,1,1)
+    }
+    return(list(ff=ff,sfg=sfg,feps=input$ruv_model,bpop=bpop_vals,d=d_vals,sigma=sigma_vals,
+                notfixed_bpop=notfixed_bpop,notfixed_d=notfixed_d,notfixed_sigma=notfixed_sigma))
+  })
   updateModel <- reactive({
-    if(input$db=="db.1") {
+    
+    if(input$model=="one.comp") {
+      source("Model.2.PK.one.comp.oral.R")
+      input$model <- poped.db.1
+      #input$facet_scales="fixed"
+      #print(plot_model_prediction(poped.db.2,IPRED=input$IPRED,DV=input$DV,separate.groups=input$separate.groups,facet_scales="free"))
+    }
+    if(input$model=="db.1") {
       source("/Users/ahooker/Documents/_PROJECTS/PopED_in_R/poped_r/models/one.comp.emax.model.POPED.R")
       source("/Users/ahooker/Documents/_PROJECTS/PopED_in_R/poped_r/models/one.comp.emax.design.POPED.R") 
       poped.db <- poped.db.2
       facet_scales="free"
       #print(plot_model_prediction(poped.db.2,IPRED=input$IPRED,DV=input$DV,separate.groups=input$separate.groups,facet_scales="free"))
     }
-    if(input$db=="db.2"){
+    if(input$model=="db.3"){
       source("/Users/ahooker/Documents/_PROJECTS/PopED_in_R/poped_r/models/warfarin.model.design.all_in_one.POPED.R") # 4-group, add+prop, as pfim
       poped.db <- create.poped.database(warfarin.design.1.red.input())
       poped.db$ga <- rbind(50,60,70,80)
@@ -37,22 +65,42 @@ shinyServer(function(input, output) {
   # Generate a plot of the requested variable against mpg and only 
   # include outliers if requested
   output$modelPlot <- renderPlot({
-    if(input$db=="db.1") {
+    #     if(input$model=="one.comp") {
+    #       source("Model.2.PK.one.comp.oral.R")
+    #       poped.db <- poped.db.1
+    #       facet_scales="fixed"
+    #       #print(plot_model_prediction(poped.db.2,IPRED=input$IPRED,DV=input$DV,separate.groups=input$separate.groups,facet_scales="free"))
+    #     }
+    if(input$model=="db.1") {
       source("/Users/ahooker/Documents/_PROJECTS/PopED_in_R/poped_r/models/one.comp.emax.model.POPED.R")
       source("/Users/ahooker/Documents/_PROJECTS/PopED_in_R/poped_r/models/one.comp.emax.design.POPED.R") 
       poped.db <- poped.db.2
       facet_scales="free"
       #print(plot_model_prediction(poped.db.2,IPRED=input$IPRED,DV=input$DV,separate.groups=input$separate.groups,facet_scales="free"))
     }
-    if(input$db=="db.2"){
+    if(input$model=="db.2"){
       source("/Users/ahooker/Documents/_PROJECTS/PopED_in_R/poped_r/models/warfarin.model.design.all_in_one.POPED.R") # 4-group, add+prop, as pfim
       poped.db <- create.poped.database(warfarin.design.1.red.input())
       poped.db$ga <- rbind(50,60,70,80)
       facet_scales="fixed"
       #print(plot_model_prediction(poped.db,IPRED=input$IPRED,DV=input$DV,separate.groups=input$separate.groups))
     }
-    #updateModel()
-    print(plot_model_prediction(poped.db,IPRED=input$IPRED,DV=input$DV,separate.groups=input$separate.groups,facet_scales=facet_scales))
-    #print(plot_model_prediction(poped.db.2,IPRED=TRUE,DV=TRUE))
+    model <- updateModel_2()
+    design <- updateDesign()
+    poped.db <- create.poped.database(list(),
+                                      ff_file=model$ff,
+                                      fError_file=model$feps,
+                                      fg_file=model$sfg,
+                                      groupsize=20,
+                                      sigma=model$sigma,
+                                      bpop=model$bpop,  
+                                      d=model$d, 
+                                      xt=design$xt,
+                                      maxxt=336, 
+                                      minxt=0, 
+                                      a=c(20,24))  
+    
+    #print(plot_model_prediction(poped.db,IPRED=input$IPRED,DV=input$DV,separate.groups=input$separate.groups,facet_scales=facet_scales))
+    print(plot_model_prediction(poped.db,IPRED=input$IPRED,DV=input$DV,separate.groups=input$separate.groups))#print(plot_model_prediction(poped.db.2,IPRED=TRUE,DV=TRUE))
   })
 })
--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -9,25 +9,43 @@ shinyUI(pageWithSidebar(
   # Sidebar with controls to select the variable to plot against mpg
   # and to specify whether outliers should be included
   sidebarPanel(
-    selectInput("db", "Model and design:",
-                list("1-comp PK, Direct effect Emax PD" = "db.1", 
+    selectInput("model", "Model:",
+                list("PK: 1-cpt, 1st order abs." = "one.comp.oral",
+                     "1-comp PK, Direct effect Emax PD" = "db.1", 
                      "Warfarin" = "db.2")),
     
+    selectInput("ruv_model", "Residual Unexplained Variability Model:",
+                list("Additive + Proportional" = "feps.add.prop")),
+    conditionalPanel(
+      condition = "input$model == one.comp.oral",
+      textInput("xt", "Sample times:", "1,2,8,16,24" )
+    )    
     ##  create plot of model 
     ## plot_model_prediction(poped.db,IPRED=T,DV=T)
     
-    checkboxInput("IPRED", "Show IPRED", FALSE),
-    
-    checkboxInput("DV", "Show DV", FALSE),
-    checkboxInput("separate.groups", "Separate Groups", FALSE)
-    
-    
+    #     checkboxInput("IPRED", "Show IPRED", FALSE),
+    #     
+    #     checkboxInput("DV", "Show DV", FALSE),
+    #     checkboxInput("separate.groups", "Separate Groups", FALSE)
   ),
   
   # Show the caption and plot of the requested variable against mpg
   mainPanel(
     #h3(textOutput("caption")),
-    
-    plotOutput("modelPlot")
+    #     checkboxInput("smooth", "Smooth"),
+    #     conditionalPanel(
+    #       condition = "input.smooth == true",
+    #       selectInput("smoothMethod", "Method",
+    #                   list("lm", "glm", "gam", "loess", "rlm"))
+    #     ),
+     tabsetPanel(
+       tabPanel("Plot", 
+                plotOutput("modelPlot"),
+                checkboxInput("IPRED", "Show IPRED", FALSE),
+                checkboxInput("DV", "Show DV", FALSE),
+                checkboxInput("separate.groups", "Separate Groups", FALSE)), 
+       tabPanel("Summary", verbatimTextOutput("summary")),
+       tabPanel("Table", tableOutput("table")))
+    #plotOutput("modelPlot")
   )
 ))
