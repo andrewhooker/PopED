5f7ff7a5ea21e176a9a1ab759d3060889a749a48
Andrew Hooker <andrew.c.hooker@gmail.com>
2015-07-15 12:47:53 +0200

    fix for categorical optimization

--- R/get_vector_str.R
+++ R/get_vector_str.R
@@ -5,13 +5,15 @@
 get_vector_str <- function(vec){
    str_vector = '' 
    if((length(vec)==1)){
-       str_vector = sprintf('%d',vec[1])
+       str_vector = sprintf('%g',vec[1])
        return(str_vector)
    } else {
        for(i in 1:length(vec)){
-           str_vector = sprintf('%s %d ',str_vector,vec[i])
+           str_vector = sprintf('%s %g ',str_vector,vec[i])
        }
        str_vector = sprintf('%s',str_vector)
    }
 return( str_vector) 
 }
+
+
