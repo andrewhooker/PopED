5046a08e8f66dc0f5d74c9cc6e8ff22487074019
Andrew Hooker <andrew.c.hooker@gmail.com>
2016-02-01 06:05:49 -0500

    update unit testing

--- tests/testthat/test_E_family.R
+++ tests/testthat/test_E_family.R
@@ -3,11 +3,12 @@ context("E-family")
 test_that("ed_laplace_ofv works", {
   
   ex_string <- ex_to_string("examples_fcn_doc/examples_ed_laplace_ofv.R",comment_dontrun=comment_dontrun)
+  writeLines(ex_string, "temp.R")
   sink("tmp.txt")
-  eval(parse(text=ex_string))
+  source("temp.R")  
   sink()
-  file.remove("tmp.txt")
-
+  file.remove("tmp.txt","temp.R")
+  
   expect_gt(output$E_ofv,1.0e+24)
     
 })
--- tests/testthat/test_optimization.R
+++ tests/testthat/test_optimization.R
@@ -3,10 +3,12 @@ context("Optimization")
 test_that("optim_ARS works", {
   
   ex_string <- ex_to_string("examples_fcn_doc/examples_optim_ARS.R",comment_dontrun=comment_dontrun)
+  writeLines(ex_string, "temp.R")
   sink("tmp.txt")
-  eval(parse(text=ex_string))
+  source("temp.R")  
   sink()
-  file.remove("tmp.txt")
+  file.remove("tmp.txt","temp.R")
+  
   
   expect_true(res1$ofv <= 159.0012) # check that things are minimizing
   
@@ -24,56 +26,61 @@ test_that("optim_ARS works", {
 
 test_that("a_line_search, mfea, poped_optimize, poped_optim and RS_opt_gen work", {
   
-  ex_string_1 <- ex_to_string("examples_fcn_doc/warfarin_optimize.R",comment_dontrun=comment_dontrun)
-  eval(parse(text=ex_string_1))
-  
+  source("examples_fcn_doc/warfarin_optimize.R")
+
   expect_warning(a_line_search(poped.db))
   
-  ex_string_2 <- ex_to_string("examples_fcn_doc/examples_a_line_search.R",comment_dontrun=comment_dontrun)
+  ex_string <- ex_to_string("examples_fcn_doc/examples_a_line_search.R",comment_dontrun=comment_dontrun)
+  writeLines(ex_string, "temp.R")
   sink("tmp.txt")
-  eval(parse(text=ex_string_2))
+  source("temp.R")  
   sink()
-  file.remove("tmp.txt")
+  file.remove("tmp.txt","temp.R")
   
   expect_true(output$best_changed)
   
-  ex_string_3 <- ex_to_string("examples_fcn_doc/examples_mfea.R",comment_dontrun=comment_dontrun)
+  ex_string <- ex_to_string("examples_fcn_doc/examples_mfea.R",comment_dontrun=comment_dontrun)
+  writeLines(ex_string, "temp.R")
   sink("tmp.txt")
-  eval(parse(text=ex_string_3))
+  source("temp.R")  
   sink()
-  file.remove("tmp.txt")
+  file.remove("tmp.txt","temp.R")
   
   expect_equivalent(out_1$poped.db$design$a[1,1],100)
   
-  ex_string_4 <- ex_to_string("examples_fcn_doc/examples_poped_optimize.R",comment_dontrun=comment_dontrun)
+  ex_string <- ex_to_string("examples_fcn_doc/examples_poped_optimize.R",comment_dontrun=comment_dontrun)
+  writeLines(ex_string, "temp.R")
   sink("tmp.txt")
-  eval(parse(text=ex_string_4))
+  source("temp.R")  
   sink()
-  file.remove("tmp.txt")
+  file.remove("tmp.txt","temp.R")
   
   expect_equivalent(out_1$poped.db$design$a[1,1],100)
   
-  ex_string_5 <- ex_to_string("examples_fcn_doc/examples_poped_optim.R",comment_dontrun=comment_dontrun)
+  ex_string <- ex_to_string("examples_fcn_doc/examples_poped_optim.R",comment_dontrun=comment_dontrun)
+  writeLines(ex_string, "temp.R")
   sink("tmp.txt")
-  eval(parse(text=ex_string_5))
+  source("temp.R")  
   sink()
-  file.remove("tmp.txt")
+  file.remove("tmp.txt","temp.R")
   
   expect_equivalent(out_1$poped.db$design$a[1,1],100)
   
-  ex_string_6 <- ex_to_string("examples_fcn_doc/examples_RS_opt_gen.R",comment_dontrun=comment_dontrun)
+  ex_string <- ex_to_string("examples_fcn_doc/examples_RS_opt_gen.R",comment_dontrun=comment_dontrun)
+  writeLines(ex_string, "temp.R")
   sink("tmp.txt")
-  eval(parse(text=ex_string_6))
+  source("temp.R")  
   sink()
-  file.remove("tmp.txt")
+  file.remove("tmp.txt","temp.R")
   
   expect_output(str(out_1),"List of 6")
   
-  ex_string_7 <- ex_to_string("examples_fcn_doc/examples_RS_opt.R",comment_dontrun=comment_dontrun)
+  ex_string <- ex_to_string("examples_fcn_doc/examples_RS_opt.R",comment_dontrun=comment_dontrun)
+  writeLines(ex_string, "temp.R")
   sink("tmp.txt")
-  eval(parse(text=ex_string_7))
+  source("temp.R")  
   sink()
-  file.remove("tmp.txt")
+  file.remove("tmp.txt","temp.R")
   
   expect_output(str(out_1),"List of 6")
   
