7406d1aaad11a7a9a08d3898b586738943304480
Andrew Hooker <andrew.c.hooker@gmail.com>
2016-02-02 05:41:55 -0500

    Update namespace

--- R/median_hilow_poped.R
+++ R/median_hilow_poped.R
@@ -1,3 +1,15 @@
+#' Wrap summary functions from Hmisc and ggplot to work with stat_summary in ggplot
+#' 
+#' Created for back compatibility with older versions of ggplot, and so that 
+#' PopED does not have to load ggplot when started.
+#' 
+#' @param x A numeric vector
+#' @param ... Additional arguments passed to \code{\link[Hmisc]{smedian.hilow}} or 
+#'   \code{\link[ggplot2]{median_hilow}}, depending on your version of ggplot.
+#' 
+#' @export
+
+
 median_hilow_poped <- function (x, ...) 
 {
   if(packageVersion("ggplot2") <= "1.0.1"){
