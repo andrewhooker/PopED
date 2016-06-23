b799c8c15fd3df2521dabb5817278cb5409079b9
Andrew Hooker <andrew.c.hooker@gmail.com>
2016-06-23 13:39:09 +0200

    Simplified for printing to screen and file

--- R/fprintf.R
+++ R/fprintf.R
@@ -1,12 +1,15 @@
 ## Function written to match MATLAB function
 ## Author: Andrew Hooker
 
-fprintf <- function(file="",...){
+fprintf <- function(file="",...,both=FALSE){
   if(any(class(file)=="file")){
     cat(sprintf(...),file=file)
   } else {
     if(file==""){cat(sprintf(...))} else {
       cat(sprintf(file,...))
     }
+  }
+  if(both && file!=""){
+    cat(sprintf(...))
   }
 }
