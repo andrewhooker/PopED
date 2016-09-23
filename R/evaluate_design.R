bc5c992b7d1cbca869661b6ec2eea5efefa672ad
Andrew Hooker <andrew.c.hooker@gmail.com>
2016-09-23 10:00:04 +0200

    New function for evaluating a design

---
+++ R/evaluate_design.R
@@ -0,0 +1,18 @@
+#' Evaluate a design
+#' 
+#' This tunction evaluates the design defined in a poped database.
+#' 
+#' @param poped.db A poped database
+#' @param ... Extra parameters passed to \code{\link{calc_ofv_and_fim}} and \code{\link{get_rse}}
+#' @return A list of elements evaluating the current design.
+#' @export
+#' 
+#' @example tests/testthat/examples_fcn_doc/warfarin_basic.R
+#' @example tests/testthat/examples_fcn_doc/examples_evaluate_design.R
+#' @family evaluate_design
+
+evaluate_design <- function(poped.db, ...) {
+  out <- calc_ofv_and_fim(poped.db,...)
+  out$rse <- get_rse(out$fim,poped.db,...)
+  return(out)
+}
