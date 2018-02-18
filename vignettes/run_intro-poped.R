67328e0b63767f6e8dc451ebb1248fe8cffcc233
giulialestini <giulia.lestini@gmail.com>
2018-02-18 16:17:29 +0100

    This program was created to run the "render" function for transforming the file "intro-poped.Rmd" into a user-friendly readable docunment "html_vignette"

---
+++ vignettes/run_intro-poped.R
@@ -0,0 +1,12 @@
+# Author: Giulia
+# Task: Run "render" function to transform intro-poped.Rmd into a docunment html_vignette
+# Output: intro-poped.html
+# Note: to run this code it is necessary to be in the same directory of the Rmd file
+# that we want to execute, in this case it is: ~/PopED/vignettes
+rm(list=ls(all=TRUE))
+library(rmarkdown)
+
+## Ensure that no library from the user home will be loaded such that
+## we run with the production packages only
+.libPaths(grep("home", .libPaths(), value=TRUE, invert=TRUE))
+render("intro-poped.Rmd")
