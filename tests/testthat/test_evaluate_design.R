b6aba8dd6fea9ecb90969727eb577dd9ddd15d94
Andrew Hooker <andrew.c.hooker@gmail.com>
2018-03-16 10:18:09 +0100

    new tests for evaluate_power

--- tests/testthat/test_evaluate_design.R
+++ tests/testthat/test_evaluate_design.R
@@ -8,3 +8,12 @@ test_that("Design evaluation works", {
   expect_output(str(evaluate_design(poped.db)),"List of 3")
 })
 
+test_that("Power evaluation works", {
+  
+  source("examples_fcn_doc/examples_evaluate_power.R")
+
+  expect_error(evaluate_power(poped.db,bpopIdx = 4))
+
+  expect_equal(evaluate_power(poped.db_2,bpopIdx = 4)$power$predPower,91.5)
+    
+})
