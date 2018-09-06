3f11f9420f9300e92fde37f6af4a504a1534004b
Andrew Hooker <andrew.c.hooker@gmail.com>
2018-09-06 14:15:50 +0200

    change 10v example to ODE model.  analytic model was not correct for chnaging parameter values betwen occasions.

--- inst/examples/ex.14.PK.IOV.R
+++ inst/examples/ex.14.PK.IOV.R
@@ -1,28 +1,55 @@
 #library(PopED)
 
 ##-- Model: One comp first order absorption
-## -- Analytic solution for both mutiple and single dosing
-ff <- function(model_switch,xt,parameters,poped.db){
+library(Rcpp)
+
+cppFunction(
+  'List one_comp_oral_ode(double Time, NumericVector A, NumericVector Pars) {
+   int n = A.size();
+   NumericVector dA(n);
+            
+   double CL_OCC_1 = Pars[0];
+   double CL_OCC_2 = Pars[1];
+   double V = Pars[2];
+   double KA = Pars[3];
+   double TAU = Pars[4];
+   double N,CL;
+            
+   N = floor(Time/TAU)+1;
+   CL = CL_OCC_1;
+   if(N>6) CL = CL_OCC_2;
+   
+   dA[0] = -KA*A[0];
+   dA[1] = KA*A[0] - (CL/V)*A[1];
+   return List::create(dA);
+   }'
+)
+
+ff.ode.rcpp <- function(model_switch, xt, parameters, poped.db){
   with(as.list(parameters),{
-    y=xt
-    N = floor(xt/TAU)+1
-    CL = (xt*0+1)*CL_OCC_1
-    CL[N>9] = CL_OCC_2
-    y=(DOSE*Favail/V)*(KA/(KA - CL/V)) * 
-      (exp(-CL/V * (xt - (N - 1) * TAU)) * (1 - exp(-N * CL/V * TAU))/(1 - exp(-CL/V * TAU)) - 
-         exp(-KA * (xt - (N - 1) * TAU)) * (1 - exp(-N * KA * TAU))/(1 - exp(-KA * TAU)))  
-    return(list( y=y,poped.db=poped.db))
+    A_ini <- c(A1=0, A2=0)
+    times_xt <- drop(xt) #xt[,,drop=T] 
+    dose_times = seq(from=0,to=max(times_xt),by=TAU)
+    eventdat <- data.frame(var = c("A1"), 
+                           time = dose_times,
+                           value = c(DOSE), method = c("add"))
+    times <- sort(c(times_xt,dose_times))
+    out <- ode(A_ini, times, one_comp_oral_ode, c(CL_OCC_1,CL_OCC_2,V,KA,TAU), 
+               events = list(data = eventdat))#atol=1e-13,rtol=1e-13)
+    y = out[, "A2"]/(V)
+    y=y[match(times_xt,out[,"time"])]
+    y=cbind(y)
+    return(list(y=y,poped.db=poped.db))
   })
 }
 
 ## -- parameter definition function 
 ## -- names match parameters in function ff
 sfg <- function(x,a,bpop,b,bocc){
-  parameters=c( V=bpop[1]*exp(b[1]),
-                KA=bpop[2]*exp(b[2]),
-                CL_OCC_1=bpop[3]*exp(b[3]+bocc[1,1]),
-                CL_OCC_2=bpop[3]*exp(b[3]+bocc[1,2]),
-                Favail=bpop[4],
+  parameters=c( CL_OCC_1=bpop[1]*exp(b[1]+bocc[1,1]),
+                CL_OCC_2=bpop[1]*exp(b[1]+bocc[1,2]),
+                V=bpop[2]*exp(b[2]),
+                KA=bpop[3]*exp(b[3]),
                 DOSE=a[1],
                 TAU=a[2])
   return( parameters ) 
@@ -30,12 +57,11 @@ sfg <- function(x,a,bpop,b,bocc){
 
 
 ## -- Define design and design space
-poped.db <- create.poped.database(ff_fun=ff,
-                                  fg_fun=sfg,
+poped.db <- create.poped.database(ff_fun=ff.ode.rcpp,
                                   fError_fun=feps.add.prop,
-                                  bpop=c(V=72.8,KA=0.25,CL=3.75,Favail=0.9), 
-                                  notfixed_bpop=c(1,1,1,0),
-                                  d=c(V=0.09,KA=0.09,CL=0.25^2), 
+                                  fg_fun=sfg,
+                                  bpop=c(CL=3.75,V=72.8,KA=0.25), 
+                                  d=c(CL=0.25^2,V=0.09,KA=0.09), 
                                   sigma=c(0.04,5e-6),
                                   notfixed_sigma=c(0,0),
                                   docc = matrix(c(0,0.09,0),nrow = 1),
@@ -49,41 +75,20 @@ poped.db <- create.poped.database(ff_fun=ff,
                                   maxa=c(DOSE=200,TAU=24),
                                   mina=c(DOSE=0,TAU=24))
 
+
+
 ##  create plot of model without variability 
 plot_model_prediction(poped.db, model_num_points = 300)
 
-##  create plot of model with variability 
-plot_model_prediction(poped.db, IPRED=T, DV=T, separate.groups=T, model_num_points = 300)
+##  Visulaize the IOV 
+library(ggplot2)
+set.seed(12345679)
+plot_model_prediction(poped.db, PRED=F,IPRED=F, 
+                      separate.groups=T, model_num_points = 500, 
+                      groupsize_sim = 1,
+                      IPRED.lines = T, alpha.IPRED.lines=0.6,
+                      sample.times = F
+) + geom_vline(xintercept = 24*6,color="red")
 
 ## evaluate initial design
 evaluate_design(poped.db)
-
-# Optimization of sample times
-output <- poped_optim(poped.db, opt_xt =TRUE, parallel=TRUE)
-
-# Evaluate optimization results
-summary(output)
-get_rse(output$FIM,output$poped.db)
-plot_model_prediction(output$poped.db)
-
-# Optimization of sample times and doses
-output_2 <- poped_optim(output$poped.db, opt_xt =TRUE, opt_a = TRUE, parallel = TRUE)
-
-summary(output_2)
-get_rse(output_2$FIM,output_2$poped.db)
-plot_model_prediction(output_2$poped.db)
-
-# Optimization of sample times with only integer time points in design space
-# faster than continuous optimization in this case
-poped.db.discrete <- create.poped.database(poped.db,discrete_xt = list(0:248))
-
-output_discrete <- poped_optim(poped.db.discrete, opt_xt=T, parallel = TRUE)
-
-
-summary(output_discrete)
-get_rse(output_discrete$FIM,output_discrete$poped.db)
-plot_model_prediction(output_discrete$poped.db)
-
-
-# Efficiency of sampling windows
-plot_efficiency_of_windows(output_discrete$poped.db, xt_windows=1)
