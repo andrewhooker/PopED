514eac08b3625b64f8793abc3b5c18e39cd598f3
Andrew Hooker <andrew.c.hooker@gmail.com>
2021-05-20 16:59:35 +0200

    Update documentation

--- vignettes/run_vignettes.R
+++ vignettes/run_vignettes.R
@@ -3,14 +3,18 @@
 # Task: Run "render" function to transform intro-poped.Rmd into a docunment "html_vignette"
 # Output: intro-poped.html
 # Note: to run this code it is necessary to Set the directory to Source file location (i.e ~/PopED/vignettes)
+
+if(interactive()) setwd(here::here("vignettes"))
 rm(list=ls(all=TRUE))
 library(rmarkdown)
 devtools::load_all("../")
+viewer <- getOption("viewer")
 
 render("intro-poped.Rmd")
-viewer <- getOption("viewer")
 viewer("intro-poped.html")
 
 render("examples.Rmd")
-viewer <- getOption("viewer")
 viewer("examples.html")
+
+#render("model_def_other_pkgs.Rmd")
+#viewer("model_def_other_pkgs.html")
